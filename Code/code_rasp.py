466310d14cbbff905b5752483e7b5a054c888f4b
RackBoy <73978995+RackBoy@users.noreply.github.com>
2021-12-03 18:37:23 -0500

    Add files via upload

---
+++ Code/code_rasp.py
@@ -0,0 +1,16 @@
+import sys 
+from sbmus import SMBus
+impor time 
+
+addr=0x08 #bus dir
+bus=SMBus(1)
+
+while True:
+	try:
+		data=bus.read_byte_data(addr)
+		print("Received: ")
+		print(data)
+		time.sleep(1)
+	except:
+		print("No received")
+		time.sleep(0.5)
